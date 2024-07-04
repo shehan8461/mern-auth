7e3dcbd170631a5f45114a045f6a71646eeb47a6
shehan8461 <shehansalitha1999@gmail.com>
2024-07-04 18:56:52 +0530

    create asign in api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,11 @@
 import User from "../models/user.model.js";
-import bcrypt from 'bcryptjs';
+import bcryptjs from 'bcryptjs';
+
+import jwt from 'jsonwebtoken'
 
 export const signup=async(req,res,next)=>{
     const {username,email,password}=req.body;
-    const hashPassword=bcrypt.hashSync(password,10)
+    const hashPassword=bcryptjs.hashSync(password,10)
     const newUser=new User({username,email,password:hashPassword});
     try{
         await newUser.save();
@@ -12,4 +14,24 @@ export const signup=async(req,res,next)=>{
         next(err);
     }
    
+}
+
+export const signin =async(req,res,next)=>{
+    const{email,password}=req.body
+    try{
+        const validUser=await User.findOne({email})
+        if(!validUser) return next(errorHandler(404,'user not found'));
+        const validPassword = bcryptjs.compareSync(password,validUser.password)
+        if(!validPassword) return next(errorHandler(401,'wrong credentials'))
+            const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+        const{password:hashedPassword,...rest}=validUser._doc;
+
+        const expiryDate = new Date(Date.now() + 24 * 60 * 60 * 1000); // 1 day from now
+        res.cookie('access_token', token, { httpOnly: true, expires: expiryDate })
+        .status(200)
+        .json(rest);
+
+    }catch(error){
+        next(error)
+    }
 }
