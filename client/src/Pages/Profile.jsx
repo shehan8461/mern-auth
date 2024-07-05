f29b0ac5bd9010d184b6e5760c9768ad3a42a0a8
shehan8461 <shehansalitha1999@gmail.com>
2024-07-06 01:24:32 +0530

    complete image upload functionality

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,19 +1,85 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { useRef } from 'react'
+
+import {getStorage, uploadBytesResumable,ref, getDownloadURL} from 'firebase/storage'
+import {app} from '../firebase';
 
 export default function Profile() {
+  const fileRef=useRef(null);
+  const [image,setImage]=useState(undefined);
+  const [imagePercent,setImagePercent]=useState(0);
+  const [imageError,setImageError]=useState(false);
+  const [formData,setFormData]=useState({});
+ 
+
+
+console.log(imagePercent)
+  console.log(image)
   const {currentUser}=useSelector((state)=>state.user)
+  useEffect(()=>{
+    if(image){
+      handleFileUpload(image)
+    }
+  },[image]);
+  const handleFileUpload=async (image)=>{
+    const storage=getStorage(app)
+    const fileName=new Date().getTime()+image.name;
+    const storageRef=ref(storage,fileName)
+    const uploadTask=uploadBytesResumable(storageRef,image)
+
+    uploadTask.on('state_changed',
+      (snapshot)=>{
+      const progress=(snapshot.bytesTransferred/snapshot.totalBytes)*100;
+      setImagePercent(Math.round(progress))
+      
+    },
+  
+    (error)=>{
+      setImageError(true)
+    },
+    ()=>{
+      getDownloadURL(uploadTask.snapshot.ref).then
+      ((downloadURL)=>setFormData({ ...formData, 
+        profilePicture:downloadURL}))
+
+      }
+    
+   )
+  };
   return (
+
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
       <form  className='flex flex-col gap-4' >
-        <img src={currentUser.profilePicture} alt='profile' className='h-24 w-24 self-center cursor-pointer rounded-full object-cover '></img>
+        <input type='file' ref={fileRef} hidden accept='image/*' onChange={(e)=>setImage(e.target.files[0])}></input>
 
-        <input defaultValue={currentUser.username} type='text' id='username' placeholder='Username' className='bg-slate-100 rounded-lg p-3'></input>
+          {/*allow read;
+                allow write:if
+                request.resource.size < 2 * 1024 * 1024 &&
+                request.resource.contentType.matches('image/.*')*/}
+
+      <img src={currentUser.profilePicture} alt='profile' className='h-24 w-24 self-center cursor-pointer rounded-full object-cover'
+      onClick={()=>fileRef.current.click()}></img>
+
+      <p className='text-sm self-center'>
+            {imageError ?(
+              <span className='text-red-700'>Error uploading image (file size must be less than 2 MB)</span>
+            ):imagePercent>0&&imagePercent<100?(
+              <span className='text-slate-700'>{`uploading:${imagePercent} %`}</span>
+            ):imagePercent===100?(
+              <span className='text-green-400'>Image uploaded successfully</span>
+            ):(
+              ''
+            )}
+
+      </p>
+
+      <input defaultValue={currentUser.username} type='text' id='username' placeholder='Username' className='bg-slate-100 rounded-lg p-3'></input>
       <input defaultValue={currentUser.email} type='email' id='email' placeholder='Email' className='bg-slate-100 rounded-lg p-3'></input>
       <input type='password' id='password' placeholder='password' className='bg-slate-100 rounded-lg p-3'></input>
 
-<button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Update</button>
+      <button className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Update</button>
 
       </form>
       <div className='flex justify-between mt-5'>
